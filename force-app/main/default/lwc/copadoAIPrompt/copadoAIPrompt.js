3598daf0361af045d99b5e7aa5e38dac36bd0895
GrahamPosey <gposey@copado.com>
2024-11-10 12:49:50 -0500

    First Commit

---
+++ force-app/main/default/lwc/copadoAIPrompt/copadoAIPrompt.js
@@ -0,0 +1,26 @@
+import { LightningElement } from 'lwc';
+import createGeneration from '@salesforce/apex/CopadoAIPromptLogic.createGeneration';
+import companyLogo from '@salesforce/resourceUrl/CopaLogo';
+
+export default class CopadoAIPrompt extends LightningElement {
+    prompt = '';
+    response = '';
+    logoUrl = companyLogo;
+
+    handlePromptChange(event) {
+        this.prompt = event.target.value;
+    }
+
+    handleClick(event) {
+        this.response = 'Working…';
+        createGeneration({ prompt: this.prompt })
+		.then(result => {
+			this.response = result;
+			this.error = undefined;
+		})
+		.catch(error => {
+            this.response = 'Error';
+			this.error = error;
+		})
+    }
+}
